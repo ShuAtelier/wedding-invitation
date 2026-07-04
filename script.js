6d7a716ec50f42a7d1d5c63419dcbb1e602d2646
ShuAtelier <shuuakmal@gmail.com>
2026-07-04 19:36:47 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const weddingDate = new Date("Dec 12, 2026 11:00:00").getTime();
+
+setInterval(() => {
+    const now = new Date().getTime();
+    const distance = weddingDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML =
+        days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+}, 1000);
